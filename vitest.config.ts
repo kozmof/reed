44dfe1f9b56023847b5ef6f6a028b78ce5db9c78
Kozo Oeda <kozo.oeda@gmail.com>
2026-04-16 11:42:19 +0000

    Add a coverage command

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,8 @@ import { configDefaults, defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     exclude: [...configDefaults.exclude, '**/perf.test.ts'],
+    coverage: {
+      provider: 'v8'
+    }
   },
 });
